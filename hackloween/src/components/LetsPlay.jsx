20b44dc26e9ea9367874ac57d904f59f2781d615
Gautier Mille <gautier.mille@gmail.com>
2019-10-30 18:57:35 +0100

    modification merge problems

--- hackloween/src/components/LetsPlay.jsx
+++ hackloween/src/components/LetsPlay.jsx
@@ -3,7 +3,6 @@ import NavBar from "./NavBar";
 import "./style/LetsPlay.scss";
 import GetMovie from "./GetMovie";
 
-
 class LetsPlay extends React.Component {
   constructor(props) {
     super(props);
@@ -43,19 +42,6 @@ class LetsPlay extends React.Component {
           <br /> {this.state.count}
         </p>
         <GetMovie />
-        <div id="quizz">
-          <div id="moviePict"></div>
-          <div id="answer">
-            <div>
-              <button className="answerCase">Answer 1</button>
-              <button className="answerCase">Answer 2</button>
-            </div>
-            <div>
-              <button className="answerCase">Answer 3</button>
-              <button className="answerCase">Answer 4</button>
-            </div>
-          </div>
-        </div>
       </div>
     );
   }

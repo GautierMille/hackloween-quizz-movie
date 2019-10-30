d698b96a7d5fcfc2b99296d47e81d64698762e5a
jervalles <jer.vallles@outlook.com>
2019-10-30 15:18:19 +0100

    Score counter State initialized

--- hackloween/src/components/LetsPlay.jsx
+++ hackloween/src/components/LetsPlay.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import NavBar from "./NavBar";
 
-function LetsPlay() {
-  return (
-    <div>
-      <NavBar />
-    </div>
-  );
+class LetsPlay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ScoreCount: 0
+    };
+  }
+
+  incrementScore = () => {
+    this.setState({ ScoreCount: this.state.ScoreCount });
+  };
+
+  render() {
+    return (
+      <div>
+        <NavBar
+          displayScore={this.state.ScoreCount}
+          Scoring={this.incrementScore}
+        />
+      </div>
+    );
+  }
 }
 
 export default LetsPlay;
--- hackloween/src/components/NavBar.jsx
+++ hackloween/src/components/NavBar.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import "./style/NavBar.scss";
 import { Link } from "react-router-dom";
 
-function NavBar() {
-  return (
-    <div className="NavBar">
-      <Link to="/">Home</Link>
-      Your Score : 400 pts
-    </div>
-  );
-}
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
+  render() {
+    return (
+      <div className="NavBar">
+        <Link to="/">Home</Link>
+        Your Score : {this.props.displayScore} pts
+      </div>
+    );
+  }
+}
 export default NavBar;

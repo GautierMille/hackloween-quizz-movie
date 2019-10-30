fb84ebbe980b5b4da97d7b2db3c1d4ce10b8cc66
LaurentMahieu <mahieu.l@hotmail.fr>
2019-10-30 20:11:26 +0100

    create div tab

--- hackloween/src/components/LetsPlay.jsx
+++ hackloween/src/components/LetsPlay.jsx
@@ -7,12 +7,36 @@ class LetsPlay extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      ScoreCount: 0
+      ScoreCount: 0,
+      blurTab: [
+        [true, true, true],
+        [true, true, true],
+        [true, false, true],
+        [true, true, true]
+      ]
     };
+  }
+
+  componentDidMount() {
+    this.blurFunction();
   }
 
   incrementScore = () => {
     this.setState({ ScoreCount: this.state.ScoreCount + 1 });
+  };
+
+  blurFunction = () => {
+    for (let i = 0; i <= 3; i++) {
+      for (let j = 0; j <= 2; j++) {
+        for (let k = 2000; k <= 20000; k = k + 2000) {
+          setTimeout(() => {
+            let arr = this.state.blurTab;
+            arr[i][j] = false;
+            this.setState({ blurTab: arr });
+          }, k);
+        }
+      }
+    }
   };
 
   render() {
@@ -23,19 +47,27 @@ class LetsPlay extends React.Component {
           Scoring={this.incrementScore}
         />
         <Countdown />
-      <div id="quizz">
-        <div id="moviePict"></div>
-        <div id="answer">
-          <div>
-            <button className="answerCase">Answer 1</button>
-            <button className="answerCase">Answer 2</button>
+        <div id="quizz">
+          <div id="moviePict">
+            {this.state.blurTab.map(row =>
+              row.map(column => {
+                return (
+                  <div className={column === true ? "blur" : "noBlur"}></div>
+                );
+              })
+            )}
           </div>
-          <div>
-            <button className="answerCase">Answer 3</button>
-            <button className="answerCase">Answer 4</button>
+          <div id="answer">
+            <div>
+              <button className="answerCase">Answer 1</button>
+              <button className="answerCase">Answer 2</button>
+            </div>
+            <div>
+              <button className="answerCase">Answer 3</button>
+              <button className="answerCase">Answer 4</button>
+            </div>
           </div>
         </div>
-      </div>
       </div>
     );
   }

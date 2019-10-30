0dad1555aa7379ba405c43de6f036f0b2a255565
LaurentMahieu <mahieu.l@hotmail.fr>
2019-10-30 13:09:42 +0100

    HomePage done

--- hackloween/src/components/HomePage.jsx
+++ hackloween/src/components/HomePage.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./style/HomePage.scss";
 
 function HomePage() {
-  return <Link to="/LetsPlay">Play</Link>;
+  return (
+    <div className="homePage">
+      <div className="title">
+        <h1>Horror Movie</h1>
+        <h1> QUIZZ</h1>
+      </div>
+      <button>
+        <Link to="/LetsPlay">
+          <p>START</p>
+        </Link>
+      </button>
+      <div className="madePart"> - Made by : Us - </div>
+    </div>
+  );
 }
 
 export default HomePage;

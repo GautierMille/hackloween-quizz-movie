90b5a9aa78730d957a697f9a41a99d739fb5237a
Gautier Mille <gautier.mille@gmail.com>
2019-10-30 16:01:41 +0100

    add random answer

--- hackloween/src/components/GetMovie.jsx
+++ hackloween/src/components/GetMovie.jsx
@@ -15,15 +15,18 @@ class GetMovie extends React.Component {
     super(props);
     this.state = {
       movies: sampleMovie,
-      firstAnswer: sampleMovie,
-      secondAnswer: sampleMovie,
-      thirdAnswer: sampleMovie,
-      fourthAnswer: sampleMovie
+      answers: [sampleMovie, sampleMovie, sampleMovie, sampleMovie],
+      answer1: sampleMovie,
+      answer2: sampleMovie,
+      answer3: sampleMovie,
+      answer4: sampleMovie,
+      goodAnswer: 0
     };
   }
   componentDidMount() {
     this.getMovie();
     this.getAnswers();
+    this.goodAnswer();
   }
   getMovie() {
     axios
@@ -39,6 +42,10 @@ class GetMovie extends React.Component {
   }
 
   getAnswers() {
+    let movie1 = Math.floor(Math.random() * Math.floor(82));
+    let movie2 = Math.floor(Math.random() * Math.floor(82));
+    let movie3 = Math.floor(Math.random() * Math.floor(82));
+    let movie4 = Math.floor(Math.random() * Math.floor(82));
     axios
       .get("https://hackathon-wild-hackoween.herokuapp.com/movies")
       // Extract the DATA from the received response
@@ -46,25 +53,34 @@ class GetMovie extends React.Component {
       // Use this data to update the state
       .then(data => {
         this.setState({
-          firstAnswer: data.movies[Math.floor(Math.random() * Math.floor(82))],
-          secondAnswer: data.movies[Math.floor(Math.random() * Math.floor(82))],
-          thirdAnswer: data.movies[Math.floor(Math.random() * Math.floor(82))],
-          fourthAnswer: data.movies[Math.floor(Math.random() * Math.floor(82))]
+          answers: [
+            data.movies[movie1],
+            data.movies[movie2],
+            data.movies[movie3],
+            data.movies[movie4]
+          ],
+          answer1: data.movies[movie1],
+          answer2: data.movies[movie2],
+          answer3: data.movies[movie3],
+          answer4: data.movies[movie4]
         });
       });
+  }
+  goodAnswer() {
+    let response = Math.floor(Math.random() * 3);
+    this.setState({ goodAnswer: response });
   }
 
   render() {
     return (
       <div>
         <MovieCard
-          posterUrl={this.state.movies.posterUrl}
-          title={this.state.movies.title}
+          posterUrl={this.state.answers[this.state.goodAnswer].posterUrl}
         ></MovieCard>
-        <button>{this.state.firstAnswer.title}</button>
-        <button>{this.state.secondAnswer.title}</button>
-        <button>{this.state.thirdAnswer.title}</button>
-        <button>{this.state.fourthAnswer.title}</button>
+        <button>{this.state.answers[0].title}`</button>
+        <button>{this.state.answers[1].title}</button>
+        <button>{this.state.answers[2].title}</button>
+        <button>{this.state.answers[3].title}</button>
       </div>
     );
   }

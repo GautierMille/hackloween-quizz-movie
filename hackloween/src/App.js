7ac114da9c629818463225d760634f82833f7428
nassilio <hubmattfac@fac.com>
2019-10-30 14:42:17 +0100

    modified App for Countdown

--- hackloween/src/App.js
+++ hackloween/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.scss";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import LetsPlay from "./components/LetsPlay";
 import Countdown from "./components/Countdown";
 
 function App() {
   return (
     <div className="App">
-      <h1>Helloween !</h1>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/LetsPlay" component={LetsPlay} />
+      </Switch>
       <Countdown />
     </div>
   );

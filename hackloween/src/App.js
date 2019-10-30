042ba21c5bde2eb5552c256a3f2289a4f8e17145
LaurentMahieu <mahieu.l@hotmail.fr>
2019-10-30 12:03:03 +0100

    initialisation router

--- hackloween/src/App.js
+++ hackloween/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.scss";
-
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import LetsPlay from "./components/LetsPlay";
 
 function App() {
   return (
     <div className="App">
-
       <h1>Helloween !</h1>
-
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/LetsPlay" component={LetsPlay} />
+      </Switch>
     </div>
   );
 }

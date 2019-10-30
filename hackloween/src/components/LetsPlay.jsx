0ca407bfb198ca4f782d905c3ed2ee50d90c5848
LaurentMahieu <mahieu.l@hotmail.fr>
2019-10-30 14:16:38 +0100

    correct syntaxe

--- hackloween/src/components/LetsPlay.jsx
+++ hackloween/src/components/LetsPlay.jsx
@@ -10,14 +10,14 @@ function LetsPlay() {
         <div id="moviePict">
           <h1>IMAGE</h1>
         </div>
-        <div id="awnser">
+        <div id="answer">
           <div>
-            <button className="awnserCase">Awnser 1</button>
-            <button className="awnserCase">Awnser 2</button>
+            <button className="answerCase">Answer 1</button>
+            <button className="answerCase">Answer 2</button>
           </div>
           <div>
-            <button className="awnserCase">Awnser 3</button>
-            <button className="awnserCase">Awnser 4</button>
+            <button className="answerCase">Answer 3</button>
+            <button className="answerCase">Answer 4</button>
           </div>
         </div>
       </div>

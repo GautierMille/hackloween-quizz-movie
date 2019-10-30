75eecf06111462797fd6fd22b38c3758afa5cc4e
Gautier Mille <gautier.mille@gmail.com>
2019-10-30 18:59:14 +0100

    delete useless div

--- hackloween/src/components/MovieCard.jsx
+++ hackloween/src/components/MovieCard.jsx
@@ -8,7 +8,7 @@ class MovieCard extends React.Component {
 
   render() {
     return (
-      <div id="moviePict">
+      <div>
         <img src={this.props.posterUrl}></img>
         <h1>{this.props.title}</h1>
       </div>

b07ce46d742192cfed54e322153db83955f2f659
jervalles <jer.vallles@outlook.com>
2019-10-30 14:10:47 +0100

    NavBar added. Home link moved from LetsPlay component to NavBar component. Score added but not automatical yet

--- hackloween/src/components/LetsPlay.jsx
+++ hackloween/src/components/LetsPlay.jsx
@@ -1,8 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import NavBar from "./NavBar";
 
 function LetsPlay() {
-  return <Link to="/">Home</Link>;
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 }
 
 export default LetsPlay;

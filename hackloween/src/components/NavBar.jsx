ebaf13af0fb4f0257e934c13c7456a737f12eb10
LaurentMahieu <mahieu.l@hotmail.fr>
2019-10-31 01:51:49 +0100

    tansform class useless to function

--- hackloween/src/components/NavBar.jsx
+++ hackloween/src/components/NavBar.jsx
@@ -2,24 +2,19 @@ import React from "react";
 import "./style/NavBar.scss";
 import { Link } from "react-router-dom";
 
-class NavBar extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="NavBar">
-        <div id="textNavBar">
+function NavBar(props) {
+  return (
+    <div className="NavBar">
+      <div id="textNavBar">
         <Link to="/">Home</Link>
         {/* TEST INCREMENT BUTTON
         <button type="button" onClick={this.props.Scoring}>
           Test Increment
         </button>*/}
-        Your Score : {this.props.displayScore} pts
+        Your Score : {props.displayScore} pts
       </div>
-      </div>
-    );
-  }
+    </div>
+  );
 }
+
 export default NavBar;
